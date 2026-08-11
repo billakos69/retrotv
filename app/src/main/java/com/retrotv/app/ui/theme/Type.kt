ef16aacdde9c2a5a89912bba20c857c97c8803a2
billakos69 <basixas@gmail.com>
2026-08-11 08:33:58 +0300

    Import Typography from androidx.tv.material3

--- app/src/main/java/com/retrotv/app/ui/theme/Type.kt
+++ app/src/main/java/com/retrotv/app/ui/theme/Type.kt
@@ -1,9 +1,9 @@
 package com.retrotv.app.ui.theme
 
-import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.tv.material3.Typography
 
 val RetroTypography = Typography(
     headlineLarge = TextStyle(
